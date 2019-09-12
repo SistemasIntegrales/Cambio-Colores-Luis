b54227c0758e1404de40f10302acc8ed017a33fb
SistemasIntegrales <digital@sistemasintegrales.mx>
2019-09-12 16:47:14 -0500

    se completo el input de archivo para cambiar el value por el archivo cargado

--- app2.js
+++ app2.js
@@ -1,14 +1,31 @@
+/*
+/////////////////////////////////
+Funciones de Branding del sistema
+/////////////////////////////////
+*/
+
 //Seteo el boton para cambiar el color
-const setCustomGradient = document.getElementById('previewBrandConfig');
+const previewBrandingConfig = document.getElementById('previewBrandConfig');
+
+//Seteo de inputs de archivos para cambiar el value
+document.getElementById('customLogo').addEventListener('change', (event) => {
+    document.getElementById('customLogoLabel').innerText = event.target.value;
+    document.getElementById('customLogoLabel').style.color = "green";
+});
+
+document.getElementById('customBackground').addEventListener('change', (event) => {
+    document.getElementById('customBackgroundLabel').innerText = event.target.value;
+    document.getElementById('customBackgroundLabel').style.color = "green";
+});
 
 //Selecciono el contenedor que cambiara de color
-const colorContenedor = document.getElementById('NavBar');
+const brandingNavbar = document.getElementById('NavBar');
 
 //Creo la función que cambiará el color
-const GradientBrandNavbar = setCustomGradient.addEventListener('click', (e) => {
+const GradientBrandNavbar = previewBrandingConfig.addEventListener('click', (e) => {
     //Saco el valor del input Color1
-    const customColor1 = document.getElementById('colorPrincipal').value;
-    const customColor2 = document.getElementById('colorSecundario').value;
-    colorContenedor.style.background = `linear-gradient(0deg, ${customColor1}, ${customColor2})`;
-    console.log(colorContenedor);
+    const primaryBrandingColor = document.getElementById('colorPrincipal').value;
+    const secondaryBrandingColor = document.getElementById('colorSecundario').value;
+    brandingNavbar.style.background = `linear-gradient(0deg, ${primaryBrandingColor}, ${secondaryBrandingColor})`;
+    console.log(brandingNavbar);
 });
