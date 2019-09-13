576ce63bfc7ba1749ded15e0d3f0b20f62405906
SistemasIntegrales <digital@sistemasintegrales.mx>
2019-09-13 07:20:06 -0500

    el formulario reacciona a la interaccion que recibe

--- app2.js
+++ app2.js
@@ -9,13 +9,18 @@ const previewBrandingConfig = document.getElementById('previewBrandConfig');
 
 //Seteo de inputs de archivos para cambiar el value
 document.getElementById('customLogo').addEventListener('change', (event) => {
-    document.getElementById('customLogoLabel').innerText = event.target.value;
-    document.getElementById('customLogoLabel').style.color = "green";
+  document.getElementById('customLogoLabel').innerText = event.target.value;
+  document.getElementById('customLogoLabel').style.color = "green";
 });
 
 document.getElementById('customBackground').addEventListener('change', (event) => {
-    document.getElementById('customBackgroundLabel').innerText = event.target.value;
-    document.getElementById('customBackgroundLabel').style.color = "green";
+  document.getElementById('customBackgroundLabel').innerText = event.target.value;
+  document.getElementById('customBackgroundLabel').style.color = "green";
+});
+
+//Si el formulario cambia activamos la previsualización
+document.getElementById('BrandConfig').addEventListener('change', (event) => {
+  document.getElementById('previewBrandConfig').disabled = false;
 });
 
 //Selecciono el contenedor que cambiara de color
@@ -23,9 +28,14 @@ const brandingNavbar = document.getElementById('NavBar');
 
 //Creo la función que cambiará el color
 const GradientBrandNavbar = previewBrandingConfig.addEventListener('click', (e) => {
-    //Saco el valor del input Color1
-    const primaryBrandingColor = document.getElementById('colorPrincipal').value;
-    const secondaryBrandingColor = document.getElementById('colorSecundario').value;
-    brandingNavbar.style.background = `linear-gradient(0deg, ${primaryBrandingColor}, ${secondaryBrandingColor})`;
-    console.log(brandingNavbar);
+  //Saco el valor del input Color1
+  const primaryBrandingColor = document.getElementById('colorPrincipal').value;
+  const secondaryBrandingColor = document.getElementById('colorSecundario').value;
+  brandingNavbar.style.background = `linear-gradient(0deg, ${primaryBrandingColor}, ${secondaryBrandingColor})`;
+
+
+  console.log(brandingNavbar);
+
+  document.getElementById('setBrandConfig').disabled = false;
+  
 });
