538da8ac547263472ba0e48e061ebbadc8bc050a
SistemasIntegrales <digital@sistemasintegrales.mx>
2019-09-13 09:07:19 -0500

    ya se cambia tanto logo como background además de ajustar lógica de UX en general

--- app2.js
+++ app2.js
@@ -4,9 +4,69 @@ Funciones de Branding del sistema
 /////////////////////////////////
 */
 
-//Seteo el boton para cambiar el color
+/* Botones */
+//Se define el botón de previsualización
 const previewBrandingConfig = document.getElementById('previewBrandConfig');
+//Se define el botón de guardar cambios
+const saveBrandingConfig = document.getElementById('setBrandConfig');
 
+/* Elementos a cambiar */
+//Se define el Navbar para cambiarle el color
+const brandingNavbar = document.getElementById('NavBar');
+
+/* Funciones y lógica */
+//Si el formulario cambia activamos la previsualización
+document.getElementById('BrandConfig').addEventListener('change', (event) => {
+  previewBrandingConfig.disabled = false;
+});
+//Al agregar el color principal se activa el color secundario
+document.getElementById('colorPrincipal').addEventListener('change', (event) => {
+  document.getElementById('colorSecundario').disabled = false;
+});
+
+//Función para previsualizar los cambios
+const funcPreviewBrandingConfig = previewBrandingConfig.addEventListener('click', (e) => {
+  
+  //Definimos los colores Principal y Secundario
+  const primaryBrandingColor = document.getElementById('colorPrincipal').value;
+  const secondaryBrandingColor = document.getElementById('colorSecundario').value;
+  
+  
+  
+  //Asignamos los colores al Navbar
+  if(secondaryBrandingColor === ''){
+     brandingNavbar.style.backgroundColor = `${primaryBrandingColor}`;
+  } else {
+     brandingNavbar.style.background = `linear-gradient(0deg, ${primaryBrandingColor}, ${secondaryBrandingColor})`;
+  }; 
+  
+  //Definimos la etiqueta logo para cambiarlo
+  const logoBranding = document.getElementById('logoBranding');
+  //Definimos el input del logo
+  const customLogo = document.getElementById('customLogo');
+  //Definimos la variable con el texto logo.png
+  const logoPng = 'logo.png';
+  //Aplicamos la imagen al elemento
+  if(customLogo.value.includes(logoPng)){
+     logoBranding.src = `images/${logoPng}`;
+  };
+    
+  //Definimos la etiqueta body para cambiarlo
+  const backgroundBranding = document.getElementById('Body');
+  //Definimos el input del Background
+  const customBackground = document.getElementById('customBackground');
+  //Definimos la variable con el texto logo.png
+  const backgroundJpg = 'background.jpg';
+  //Aplicamos la imagen al elemento
+  if(customBackground.value.includes(backgroundJpg)){
+     backgroundBranding.style.backgroundImage = `url('images/${backgroundJpg}')`;
+  }
+  
+  //Cambiamos el estado del botón de guardar configuración
+  saveBrandingConfig.disabled = false;
+});
+
+/* Estilos */
 //Seteo de inputs de archivos para cambiar el value
 document.getElementById('customLogo').addEventListener('change', (event) => {
   document.getElementById('customLogoLabel').innerText = event.target.value;
@@ -18,24 +78,3 @@ document.getElementById('customBackground').addEventListener('change', (event) =
   document.getElementById('customBackgroundLabel').style.color = "green";
 });
 
-//Si el formulario cambia activamos la previsualización
-document.getElementById('BrandConfig').addEventListener('change', (event) => {
-  document.getElementById('previewBrandConfig').disabled = false;
-});
-
-//Selecciono el contenedor que cambiara de color
-const brandingNavbar = document.getElementById('NavBar');
-
-//Creo la función que cambiará el color
-const GradientBrandNavbar = previewBrandingConfig.addEventListener('click', (e) => {
-  //Saco el valor del input Color1
-  const primaryBrandingColor = document.getElementById('colorPrincipal').value;
-  const secondaryBrandingColor = document.getElementById('colorSecundario').value;
-  brandingNavbar.style.background = `linear-gradient(0deg, ${primaryBrandingColor}, ${secondaryBrandingColor})`;
-
-
-  console.log(brandingNavbar);
-
-  document.getElementById('setBrandConfig').disabled = false;
-  
-});
